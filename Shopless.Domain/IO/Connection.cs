c3511492a2d1fa7f84fff3e5e726f2d39beb5ce5
Dmitry Nogin <v-dmnogi@microsoft.com>
2019-09-13 16:33:00 -0700

    Refactor for clarity

--- Shopless.Domain/IO/Connection.cs
+++ Shopless.Domain/IO/Connection.cs
@@ -9,17 +9,14 @@ namespace Shopless.IO
     public sealed class Connection : ValueObject<Connection>
     {
         public static implicit operator Connection(string @string) => Parse(@string);
-        public static Connection Parse(string @string) => new Connection(@string);            
-
-        Connection(string @string)
-        {
-            Values = @string.Split(';')
+        public static Connection Parse(string @string) => 
+            new Connection(@string.Split(';')
                .Select(nv => nv.Split('='))
                .ToDictionary(
-                    nv => nv[0].Trim(), 
-                    nv => string.Join("=", nv.Skip(1)).Trim());
-        }
+                    nv => nv[0].Trim(),
+                    nv => string.Join("=", nv.Skip(1)).Trim()));            
 
+        Connection(IReadOnlyDictionary<string, string> values) => Values = values;
         IReadOnlyDictionary<string, string> Values { get; }
         
         public T Get<T>(string name, T @default = default(T)) =>
