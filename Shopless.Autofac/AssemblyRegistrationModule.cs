846df85aeb91aba77fa476863b34169981353b0b
Dmitry Nogin <dmitryn@mail.com>
2019-09-15 20:32:27 -0700

    Define Autofac adaptor

---
+++ Shopless.Autofac/AssemblyRegistrationModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace Shopless
+{
+    public class AssemblyRegistrationModule : Module
+    {
+        public AssemblyRegistrationModule(IEnumerable<Assembly> assemblies)
+            : this(assemblies.ToArray())
+        {
+        }
+
+        public AssemblyRegistrationModule(params Assembly[] assemblies)
+        {
+            Assemblies = assemblies;
+        }
+
+        IEnumerable<Assembly> Assemblies { get; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            foreach (var assembly in Assemblies)
+                builder.RegisterAssembly(assembly);
+        }
+    }
+}
